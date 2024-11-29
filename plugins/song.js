a76991bd931647d1a1cbf668da92af273b4e10df
Randimasl <117644553+Randimasl@users.noreply.github.com>
2024-11-30 00:18:27 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -1,96 +1,31 @@
-const {cmd , commands} = require('../command')
-const fg = require('api-dylux')
-const yts = require('yt-search')
-cmd({
-    pattern: "song",
-    desc: "To download songs.",
-    react: "🎧",
-    category: "download",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!q) return reply("Please give me a url or title")  
-const search = await yts(q)
-const data = search.videos[0];
-const url = data.url
-    
-    
-let desc = `
-⫷⦁[ * '-'_MANISHA-MD-SONG_DOWNLOADER_'-' * ]⦁⫸
-
-*🎧 MANISHA-MD SONG DOWNLOADER . .⚙️*
-
-➥ *Title:* ${data.title} 
-➥ *Duration:* ${data.timestamp} 
-➥ *Views:* ${data.views} 
-➥ *Uploaded On:* ${data.ago} 
-➥ *Link:* ${data.url} 
-
-> *©𝗠𝗔𝗡𝗜𝗦𝗛𝗔-𝗠𝗗*
-`
-
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
-
-//download audio
-
-let down = await fg.yta(url)
-let downloadUrl = down.dl_url
-
-//send audio message
-await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*©𝗠𝗔𝗡𝗜𝗦𝗛𝗔-𝗠𝗗*"},{quoted:mek})
-
-}catch(e){
-console.log(e)
-  reply('𝐶𝑎𝑛𝑡 𝐹𝑖𝑛𝑑 α ѕσηg')
-}
-})
-
-//====================VIDEO_dl=======================
-
-cmd({
-    pattern: "video",
-    desc: "To download videos.",
-    react: "🎬",
-    category: "download",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!q) return reply("Please give me a url or title")  
-const search = await yts(q)
-const data = search.videos[0];
-const url = data.url
-    
-    
-let desc = `
-⫷⦁[ * '-'_MANISHA-MD-VIDEO_DOWNLOADER_'-' * ]⦁⫸ 
-
-*🎬 MANISHA-MD VIDEO DOWNLOADER . .⚙️
-
-➥ *Title:* ${data.title} 
-➥ *Duration:* ${data.timestamp} 
-➥ *Views:* ${data.views} 
-➥ *Uploaded On:* ${data.ago} 
-➥ *Link:* ${data.url} 
-
-> *©𝗠𝗔𝗡𝗜𝗦𝗛𝗔-𝗠𝗗*
-`
-
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
-
-//download video
-
-let down = await fg.ytv(url)
-let downloadUrl = down.dl_url
-
-//send video message
-await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"*©𝗠𝗔𝗡𝗜𝗦𝗛𝗔-𝗠𝗗*"},{quoted:mek})
-
-}catch(e){
-console.log(e)
-  reply('𝐶𝑎𝑛𝑡 𝐹𝑖𝑛𝑑 α νι∂єσ')
-}
-})
+{
+  "status": true,
+  "creator": "@DarkYasiya",
+  "result": {
+    "title": "Nilan Hettiarachchi - ලෙලෙනා | Lelena (Lyrics)",
+    "thumbnail": "https://i.ytimg.com/vi/AFqtArWpv-w/0.jpg",
+    "author": null,
+    "video": {
+      "360p": {
+        "quality": "360p",
+        "dl_link": "https://dl206.filemate27.shop/?file=M3R4SUNiN3JsOHJ6WWQ2a3NQS1Y5ZGlxVlZIOCtyZ0trZGs3eVZzTEJMOVlyNHc5MXVlb0lNRkdaZXhLNk5ybk00Z1IxeHFkVklET0Vsbk1sTHhqWnlQRTFZQjRtMjZFdUtJbFZjeC9WMTNVZ3ZTcWxTSkoybEdsYnBiQUJld1FhMzUzcG45ejBqS2JrYlNHbnd6M3BtbTRwVWpHUGdJSG9tOU9iOU9FdE0xdTlFL2FlOWIyNUlBVDRER2dzY3daNnI2UXBGV3d4TGN4NWVsR0dFRnNZNWxQM0k3ODB2V1I5QkJLMHNCTWd4ancvcVMxRDRRd1FmeklLMklrYlhWUCt2bXlXQmxL",
+        "size": "9.1 MB"
+      },
+      "720p HFR": {
+        "quality": "720p",
+        "dl_link": "https://dl196.filemate11.shop/?file=M3R4SUNiN3JsOHJ6WWQ2a3NQS1Y5ZGlxVlZIOCtyZ0trZGs3eVZzTEJMOVlyNHc5MXVlb0lNRkdaZXhLNk5ybk00Z1IxeHFkVklET0Vsbk1sTHhqWnlQRTFZQjRtMjZFdUtJbFZjeC9WMTNVZ3ZTcWxTSkozbFdsYnZEckovWmJmU005cFVsdjFobUR6ZVhjNUZDcWxXU2lzbG1XYmlGUDRCMEdMYUdWbzdsTWtEdnRUdEgxeDdFWG1qYUpyNDEyamFMTnBDZmU4Znd6cUlneldVRjdMcVZ1bjUvdHovbmVvRk1Ralp4Qml4cjMrN3YyWDh0aFMrT05jQzExWTNCUXNiaTlEa2xKaVRZSXBXaThvZz09",
+        "size": "28.4 MB"
+      },
+      "1080p HFR": {
+        "quality": "1080p",
+        "dl_link": "https://dl206.filemate27.shop/?file=M3R4SUNiN3JsOHJ6WWQ2a3NQS1Y5ZGlxVlZIOCtyZ0trZGs3eVZzTEJMOVlyNHc5MXVlb0lNRkdaZXhLNk5ybk00Z1IxeHFkVklET0Vsbk1sTHhqWnlQRTFZQjRtMjZFdUtJbFZjeC9WMTNVZ3ZTcWxTSkoyRmV0THNqbE00b1lZR2N2NVVacTN5T3MzUERBc1VpOTRWdTBxRmlYZVNZUDkya3RNT1dFdE0xdWhTeVpTZGJDd29Ra3Z4R1A5TmRlNWFITjdGSG01Wm9NOXRGbENnSnpkcDhBN0ttejJ1amNyRlVjbEpBUjN4RDEvckgwWDg1c1NmSEpQeVI1SkNkVXZiNjRCVXhKbkhaWTYzcmo4cTF4",
+        "size": "39.8 MB"
+      },
+      "auto": {
+        "quality": "auto",
+        "dl_link": "https://dl206.filemate29.shop/?file=M3R4SUNiN3JsOHJ6WWQ2a3NQS1Y5ZGlxVlZIOCtyZ0trZGs3eVZzTEJMOVlyNHc5MXVlb0lNRkdaZXhLNk5ybk00Z1IxeHFkVklET0Vsbk1sTHhqWnlQRTFZQjRtMjZFdUtJbFZjeC9WMTNVZ3ZTcWxTSkoyRmV0THNqbE00b1lZR2N2NVVacTN5T3MzUERBc1VpOTRWdTBxRmlYZVNZUDkya3RNT1dFdE0xdWhTeVpTZGJDd29Ra3Z4R1A5TmRlNWFITjdGSG01Wm9NOXRGbENnSnpkcDhBN0ttejJ1amNyRlVjbEpBUjN4RDEvckgwWDg1c1NmSEpQeVI1SkNkVXZiNjRCVXhKbkhaWTYzcmo4cTF4",
+        "size": ""
+      }
+    }
+  }
+        }
